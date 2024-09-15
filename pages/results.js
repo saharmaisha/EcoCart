83d8d2df6c5c790881bdea5a81f644d891d03c62
Jennifer <jenniferrzhuu@gmail.com>
2024-09-15 04:05:28 -0700

    Added Back Button to Results Page

--- pages/results.js
+++ pages/results.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { SignOutButton } from '@clerk/nextjs';
+import styles from '../styles/Home.module.css';
 import { Container, Row, Col, Button, Card } from 'react-bootstrap'; // Import Bootstrap components
 import { GlobalStyle } from '../components/styles'; // Assuming GlobalStyle is correctly defined
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
@@ -15,9 +17,24 @@ export default function Results() {
     results = JSON.parse(data); // Parse the results passed via the query
   }
 
+  const handleGoBack = () => {
+    router.push('/');
+  };
+
   return (
     <Container>
       <GlobalStyle />
+      <div> 
+          <nav className="navbar navbar-light bg-transparent" style={{ position: 'fixed', top: 0, left: 10, backgroundColor: 'transparent', border: 'none' }}>
+            <a className="navbar-brand" href="#">
+              <img src="/imgs/Logo.png" width="85" height="85" alt="Logo" style={{ display: 'block' }} />
+            </a>
+          </nav> 
+          <div style={{ position: 'fixed', top: 35, right: 40, display: 'flex', alignItems: 'center' }}>
+          <button className={styles.button} onClick={handleGoBack} style={{ position: 'fixed', top: 35, right: 135}}>Back</button>
+          <SignOutButton><button className={styles.button} style={{ position: 'fixed', top: 35, right: 20}}>Sign Out</button></SignOutButton>
+          </div> 
+      </div>
       <Row className="justify-content-center my-4">
         <Col md={8}>
           <h1 className="text-center" style={{ color: '#2C5F34', fontSize: '4rem' }}>Results</h1>
